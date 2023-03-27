e6e11547a3207861b38e1ea3671a67b026c03c59
Sanjay <sanjay8874002308@gmail.com>
2023-03-27 17:20:24 +0530

    Solid pattern code with *

---
+++ Pattern_Practice.java
@@ -0,0 +1,17 @@
+// practice for loop--> pattern
+// Solid Pattern
+public class Pattern_Practice {
+    public static void main(String[] args) {
+
+        int n = 4;
+        int m = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
