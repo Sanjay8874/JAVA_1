a1086cea5b7070379ef58ce78eb9806ce9e98dc3
Sanjay <sanjay8874002308@gmail.com>
2023-03-28 06:38:55 +0530

    added function for add 2 numebr

--- Method.java
+++ Method.java
@@ -1,19 +1,29 @@
 import java.util.*;
 public class Method{
-
-public static void PrintName(String name){
+// function to print Name which entered from user.
+/*public static void PrintName(String name){
     System.out.println(name);
     return;
-        }
+        }*/
 
+// Function for Sum of 2 number
+
+public static int Sum(int a,  int b)
+{
+    int sum = a+b;
+    return sum;
+}
+
+//main function
  public static void main(String args[]){
-    System.out.println("Enter name");
+
     Scanner N1 = new Scanner(System.in);
-
-    String name = N1.next();
-    PrintName(name);
-
-
+    System.out.println("Enter 1st Number");
+    int a = N1.nextInt();
+    System.out.println("Enter 2nd  Number");
+    int b = N1.nextInt();
+    int sum = Sum(a,b);
+    System.out.println("Sum of 2 number : " +sum);
 
         }
 }
